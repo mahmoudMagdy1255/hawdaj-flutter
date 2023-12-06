fd83f93966add1672d85ebdbe6827b8dc8c58ff9
omarM <omarmail4contact@gmail.com>
2023-12-06 19:48:46 +0200

    colors

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class ColorsData {
+  static const Color greyscale900 = Color(0xFF212121);
+  static const Color greyscale700 = Color(0xFF616161);
+  static const Color greyscale800 = Color(0xFF424242);
+  static const Color greyscale50 = Color(0xFFFAFAFA);
+  static const Color greyscale200 = Color(0xFFEEEEEE);
+  static const Color greyscale100 = Color(0xFFF5F5F5);
+  static const Color greyscale400 = Color(0xffb4b4b4);
+  static const Color greyscale500 = Color(0xFF8A8A8A);
+  static const Color greyscale600 = Color(0xFF757575);
+  static const Color primary500 = Color(0xFF6c0a6b);
+  static const Color primary500_20 = Color(0x33284A6E);
+  static const Color primary100 = Color(0xFFE9F0FF);
+  static const Color primary500_8 = Color(0x1A246BFD);
+  static const Color grey = Color(0xFFEEEEEE);
+  static const Color success_green = Color(0xFF00ADA8);
+  static const Color error_red = Color(0xFFD72F5A);
+  static const Color warring = Color(0xFFE88733);
+
+  // static const MaterialColor myPrimaryColor = MaterialColor(
+  //   0xFF284A6E,
+  //   <int, Color>{
+  //     50: Color(0xD284A6E),
+  //     100: Color(0x1A284A6E),
+  //     200: Color(0x33284A6E),
+  //     300: Color(0x4D284A6E),
+  //     400: Color(0x66284A6E),
+  //     500: Color(0x80284A6E),
+  //     600: Color(0x99284A6E),
+  //     700: Color(0xB3284A6E),
+  //     800: Color(0xCC284A6E),
+  //     900: Color(0xE6284A6E),
+  //   },
+  // );
+}
